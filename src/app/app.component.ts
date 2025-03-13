c5f4d386cef77f80415cb1863e0ce13c0c86d4b4
Nemesis502 <klawes-bastian@t-online.de>
2025-03-13 18:19:46 +0100

    added fonts, added c Main-, lading-,navbar-content. First SCCS for Landing-page and Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
