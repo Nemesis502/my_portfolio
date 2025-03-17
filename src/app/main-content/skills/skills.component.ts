416af02002b0c9bfeadc5b380c3560c7cec97a26
Nemesis502 <klawes-bastian@t-online.de>
2025-03-17 19:12:05 +0100

    finish left side of skill, almost finish right side, mindset over not finish

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -8,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  skillSet = [
+    {
+      skillImg: "HTML",
+      name: "HTML",
+    },
+    {
+      skillImg: "CSS",
+      name: "CSS",
+    },
+    {
+      skillImg: "JavaScript",
+      name: "JavaScript",
+    },
+    {
+      skillImg: "Material Design",
+      name: "Material Design",
+    },
+    {
+      skillImg: "TypeScript",
+      name: "TypeScript",
+    },
+    {
+      skillImg: "Angular",
+      name: "Angular",
+    },
+    {
+      skillImg: "Firebase",
+      name: "Firebase",
+    },
+    {
+      skillImg: "Git",
+      name: "Git",
+    },
+    {
+      skillImg: "Rest-Api",
+      name: "Rest-Api",
+    },
+    {
+      skillImg: "Scrum",
+      name: "Scrum",
+    },
+    {
+      skillImg: "GrowthMindset",
+      name: "GrowthMindset",
+    }
+  ]
 
 }
