ad59116a1f887b86c4dd073ee06f5e54489311dd
Nemesis502 <klawes-bastian@t-online.de>
2025-03-20 19:20:09 +0100

    added  tooltips , not finish SCSS

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,74 +1,59 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, ViewEncapsulation} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  
+
   imports: [
     MatFormFieldModule,
     MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
-    MatTooltipModule,
-    
+    CommonModule
   ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
   encapsulation: ViewEncapsulation.None
-  
+
 })
 export class SkillsComponent {
   skillSet = [
     {
-      skillImg: "HTML",
       name: "HTML",
     },
     {
-      skillImg: "CSS",
       name: "CSS",
     },
     {
-      skillImg: "JavaScript",
       name: "JavaScript",
     },
     {
-      skillImg: "Material Design",
       name: "Material Design",
     },
     {
-      skillImg: "TypeScript",
       name: "TypeScript",
     },
     {
-      skillImg: "Angular",
       name: "Angular",
     },
     {
-      skillImg: "Firebase",
       name: "Firebase",
     },
     {
-      skillImg: "Git",
       name: "Git",
     },
     {
-      skillImg: "Rest-Api",
       name: "Rest-Api",
     },
     {
-      skillImg: "Scrum",
       name: "Scrum",
     },
     {
-      skillImg: "GrowthMindset",
       name: "GrowthMindset",
     }
   ]
-
-  positionOptions: TooltipPosition[] = ['above'];
-  position = new FormControl(this.positionOptions[0]);
 }
