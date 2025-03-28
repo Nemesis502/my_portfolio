7dfe40d6dd65c8ff985aeaa7dae7f72e34a1343b
Nemesis502 <klawes-bastian@t-online.de>
2025-03-28 22:26:12 +0100

    some SCCS for the references and added navigator function for dots

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
+  totalReferences: number = 3;
+  activeReferenceIndex: number = 0;
 
+  nextReferences(direction: string) {
+    if (direction === 'left') {
+      this.activeReferenceIndex =
+        (this.activeReferenceIndex - 1 + this.totalReferences) % this.totalReferences;
+    } else if (direction === 'right') {
+      this.activeReferenceIndex =
+        (this.activeReferenceIndex + 1) % this.totalReferences;
+    }
+  }
 }
+
