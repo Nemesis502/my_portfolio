21eb173328440fba2f4aa6db719089c2c0d67c29
Nemesis502 <klawes-bastian@t-online.de>
2025-03-29 22:22:26 +0100

    references container finish but animatzion are not. first steps for contact form

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { FeaturedProjectsComponent } from "./featured-projects/featured-projects.component";
 import { ReferencesComponent } from "./references/references.component";
+import { ContactMeComponent } from "./contact-me/contact-me.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, FeaturedProjectsComponent, ReferencesComponent],
+  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, FeaturedProjectsComponent, ReferencesComponent, ContactMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadZone } from 'zone.js/lib/zone';
+
+interface ReferencesComments {
+  name: string;
+  rang: string;
+  comments: string;
+}
 
 @Component({
   selector: 'app-references',
@@ -11,6 +18,99 @@ import { Component } from '@angular/core';
 export class ReferencesComponent {
   totalReferences: number = 3;
   activeReferenceIndex: number = 0;
+  selectedReference: ReferencesComments | null = null;
+  referencesComments: ReferencesComments[] = [];
+
+  constructor() {
+    this.loadReferences()
+  }
+
+  firstReferencesComments: ReferencesComments[] = [
+    {
+      name: "W.Fröhlich",
+      rang: "Team Partner",
+      comments: `Our project benefited enormously 
+         from Bastian efficient way of
+         working.`
+    },
+    {
+      name: "R.Matthes",
+      rang: "Team Partner",
+      comments: ` Bastian has proven to be a reliable group partner. His technical
+            skills and proactive approach were crucial to the success of our
+            project.`
+    },
+    {
+      name: "D.Fuhrmann",
+      rang: "Team Partner",
+      comments: ` I had the good fortune of working with Lukas in a group project at
+            the Developer Akademie that involved a lot of effort. He always
+            stayed calm, cool, and focused, and made sure our team was set up
+            for success. He's super knowledgeable, easy to work with, and I'd
+            happily work with him again given the chance.`
+    }
+  ];
+  secondReferencesComments: ReferencesComments[] = [
+    {
+      name: "R.Matthes",
+      rang: "Team Partner",
+      comments: ` Bastian has proven to be a reliable group partner. His technical
+            skills and proactive approach were crucial to the success of our
+            project.`
+    },
+    {
+      name: "D.Fuhrmann",
+      rang: "Team Partner",
+      comments: ` I had the good fortune of working with Lukas in a group project at
+            the Developer Akademie that involved a lot of effort. He always
+            stayed calm, cool, and focused, and made sure our team was set up
+            for success. He's super knowledgeable, easy to work with, and I'd
+            happily work with him again given the chance.`
+    },
+    {
+      name: "W.Fröhlich",
+      rang: "Team Partner",
+      comments: `Our project benefited enormously 
+         from Bastian efficient way of
+         working.`
+    },
+
+  ];
+  thirdReferencesComments: ReferencesComments[] = [
+    {
+      name: "D.Fuhrmann",
+      rang: "Team Partner",
+      comments: ` I had the good fortune of working with Lukas in a group project at
+            the Developer Akademie that involved a lot of effort. He always
+            stayed calm, cool, and focused, and made sure our team was set up
+            for success. He's super knowledgeable, easy to work with, and I'd
+            happily work with him again given the chance.`
+    },
+    {
+      name: "W.Fröhlich",
+      rang: "Team Partner",
+      comments: `Our project benefited enormously 
+         from Bastian efficient way of
+         working.`
+    },
+    {
+      name: "R.Matthes",
+      rang: "Team Partner",
+      comments: ` Bastian has proven to be a reliable group partner. His technical
+            skills and proactive approach were crucial to the success of our
+            project.`
+    },
+  ];
+
+  loadReferences() {
+    if (this.activeReferenceIndex == 0) {
+      this.referencesComments = this.firstReferencesComments;
+    } else if (this.activeReferenceIndex == 1) {
+      this.referencesComments = this.secondReferencesComments;
+    } else {
+      this.referencesComments = this.thirdReferencesComments;
+    }
+  }
 
   nextReferences(direction: string) {
     if (direction === 'left') {
@@ -20,6 +120,8 @@ export class ReferencesComponent {
       this.activeReferenceIndex =
         (this.activeReferenceIndex + 1) % this.totalReferences;
     }
+    this.loadReferences();
   }
+
 }
 
