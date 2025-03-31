269ec5aa0c275c73a521f5e57c2bf46402fc46cf
Nemesis502 <klawes-bastian@t-online.de>
2025-03-31 18:33:55 +0200

    work on Formular Structur and Logic

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule, getLocaleFirstDayOfWeek } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+  buttonChecked = false
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
 
+
+
+  checkButtonPolicy() {
+    this.buttonChecked = true
+  }
+
+  checkFormular(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    } else {
+      console.log("NÖ");
+      
+    }
+  }
 }
