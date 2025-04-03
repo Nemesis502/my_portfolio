91564a7ec14421bda40656d3c9b507fcac1ee032
Nemesis502 <klawes-bastian@t-online.de>
2025-04-03 18:26:51 +0200

    finish structur privacypolicy. back button on function. upload to website

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-imprint',
@@ -7,14 +7,5 @@ import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
-export class ImprintComponent implements OnInit, OnDestroy {
-  constructor(private renderer: Renderer2) {}
-
-  ngOnInit(): void {
-    this.renderer.addClass(document.body, 'imprint-body');
-  }
-
-  ngOnDestroy(): void {
-    this.renderer.removeClass(document.body, 'imprint-body');
-  }
+export class ImprintComponent {
 }
