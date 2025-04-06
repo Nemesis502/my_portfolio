13a80662c712dd7bf24d24fb8fa31e151dc68889
Nemesis502 <klawes-bastian@t-online.de>
2025-04-06 20:05:38 +0200

    added more animation in landing-page/aboute me and skills

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  @ViewChild('animImg') animImg!: ElementRef<HTMLImageElement>;
+  animationPersisted = false;
 
+  staticAnimation() {
+    this.animationPersisted = true;
+  }
 }
