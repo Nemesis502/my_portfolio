0f7a139951a7a73f9938acfd64d10c66aa87610a
Nemesis502 <klawes-bastian@t-online.de>
2025-04-15 19:28:05 +0200

    work on more responisve, finish navbar / menu. working now on landing-page

--- src/app/main-content/landing-page/navbar/navbar.component.ts
+++ src/app/main-content/landing-page/navbar/navbar.component.ts
@@ -1,21 +1,29 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component, EventEmitter, Output, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
 
+interface Options {
+  offset: number;
+  align?: "top" | "middle" | "bottom" | undefined;
+  ease?: string | undefined;
+  duration?: number | undefined;
+}
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule, CommonModule, TranslatePipe, TranslateDirective],
+  imports: [FormsModule, CommonModule, TranslatePipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   currentLanguage = "en"
   defaultResponsiveLogo = true;
-  @Output() chosenLanguage = new EventEmitter<string>();
+  menuIsClose = true;
+  dialogVisible = false;
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private viewportScroller: ViewportScroller) {
     this.translate.addLangs(['de', 'en']);
     this.translate.setDefaultLang('en');
     this.translate.use('en');
@@ -23,7 +31,21 @@ export class NavbarComponent {
   changeLanguage(language: string) {
     this.currentLanguage = language === 'en' ? 'de' : 'en';
     this.translate.use(this.currentLanguage);
-    console.log(this.currentLanguage);
-    // this.chosenLanguage.emit(this.currentLanguage)
   }
+
+  changeLogo() {
+    this.defaultResponsiveLogo = !this.defaultResponsiveLogo;
+  }
+
+  switchMenu(){
+    this.menuIsClose = !this.menuIsClose;
+    this.dialogVisible = !this.dialogVisible;
+  }
+  
+  scrollToSection(id: string):void {
+    this.viewportScroller.scrollToAnchor(id);
+    this.menuIsClose = !this.menuIsClose;
+    this.dialogVisible = !this.dialogVisible;
+  }
+
 }
